e84b4acf9b3eee8c04719e991a64f858804ea14b
Alex McRitchie <amcritchie@gmail.com>
2014-08-12 15:31:59 -0600

    Can now add tasks to task_lists.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,32 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+//$(document).ready(function () {
+//        $('#about-link').on('click', function() {
+//                alert('hello')
+//            }
+//        )
+//    }
+//)
+//
+//$(document).ready(function () {
+//        $('.test_class').on('click', function() {
+//                alert('class')
+//            }
+//        )
+//    }
+//)
+//$(document).ready(function () {
+//        $('#test_id').on('click', function() {
+//                alert('id')
+//            }
+//        )
+//    }
+//)
+//
+//$('test_id').onmouseover(fun)
+//
+//
+//$('#about-link').remove()
+//
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,9 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.order(:body)
+  end
+
   def new
     @task_list = TaskList.find(params[:task_list_id])
     @task = Task.new
@@ -7,16 +12,12 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new
-    @task.name = params[:task][:description]
-
-    if @task.valid?
-      if @task.save
-        flash[:success] = "Task was created."
-        redirect_to "/"
-      else
-        @task
-        render :new
-      end
+    @task.body= params[:task][:body]
+    @task.due_date = Date.new(params[:due_date][:year].to_i, params[:due_date][:month].to_i, params[:due_date][:day].to_i)
+    @task.task_list_id = params[:task_list_id]
+    if @task.save
+      flash[:success] = "Task was created."
+      redirect_to "/"
     else
       flash[:error] = "Your task could not be created."
       render :new
